e4eec2e4b4a899a7a20ae8997df7130f36505e39
Anya Kaygorodova <anutakay@gmail.com>
2015-04-26 21:36:32 +0700

    Добавлен шаблонизатор

--- app.js
+++ app.js
@@ -1,23 +1,29 @@
-var express = require('express')
+var express = require('express');
 
 var app = express.createServer();
 
-app.get('/', function(req, res){
+app.get('/', function(req, res) {
     res.send('hello world');
 });
 
-app.configure(function(){
-    app.use(express.methodOverride());
-    app.use(express.bodyParser());
+app.configure(function() {
+    app.use( express.methodOverride() );
+    app.use( express.bodyParser() );
     app.use(app.router);
+
+    app.set('view engine', 'ejs');
 });
 
-app.configure('development', function(){
-  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+app.configure('development', function() {
+  app.use( express.errorHandler({ dumpExceptions: true, showStack: true }) );
 });
 
-app.configure('production', function(){
-  app.use(express.errorHandler());
+app.configure('production', function() {
+  app.use( express.errorHandler() );
 });
 
-app.listen(3000);
+app.listen(3000, function() {
+  console.log("Express server listening on port %d in %s mode",
+              app.address().port,
+              app.settings.env);
+});
