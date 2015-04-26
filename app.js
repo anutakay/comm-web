af4f26d8c99b1cc34a405ea3a4bc4e721d1a5422
Anya Kaygorodova <anutakay@gmail.com>
2015-04-26 21:10:29 +0700

    Добавлена конфигурация

--- app.js
+++ app.js
@@ -1,7 +1,23 @@
-var app = require('express').createServer();
+var express = require('express')
+
+var app = express.createServer();
 
 app.get('/', function(req, res){
     res.send('hello world');
+});
+
+app.configure(function(){
+    app.use(express.methodOverride());
+    app.use(express.bodyParser());
+    app.use(app.router);
+});
+
+app.configure('development', function(){
+  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+});
+
+app.configure('production', function(){
+  app.use(express.errorHandler());
 });
 
 app.listen(3000);
